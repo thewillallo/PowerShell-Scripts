66820763b9efa404876292806ab62aecf38944e8
thewillallo <39629482+thewillallo@users.noreply.github.com>
2018-05-25 15:26:01 -0400

    Corrected issue with param
    $mailboxes was set to string instead of array, also brushed up formatting

--- Add-Calendar-Full-Access-Permissions-Bulk.ps1
+++ Add-Calendar-Full-Access-Permissions-Bulk.ps1
@@ -1,4 +1,4 @@
-function AddFullAccessMailboxPermission {
+function Add-CalendarFullAccess {
     <#
     .SYNOPSIS
     This script will set allow you to quickly give a user full access to all mailboxes or calendars that have similar naming conventions.
@@ -7,15 +7,30 @@ function AddFullAccessMailboxPermission {
     if you would like to add someone to all mailboxes that begin with a certain letter, or contain a certain string. You will need to be logged into 
     Exchange Online PowerShell in order to run this function.
     #>
-    param(
+    [CmdletBinding()]
+    param
+    (
         # $mailboxes - Retrieves all mailboxes in the environment matching a user identified string 
         # Example - {name -like "*Room*"}
-        [string]$mailboxes = Get-Mailbox -Filter {name -like "<enter conference room name similarity unique to your environment>"}
+        [array]$mailboxes,
         # $user - Set the user that will be grant full access
-        [string]$user = Read-Host "What is the username of the user that you would like to grant full access to all mailboxes?"
-
+        [string]$user,
+        # $logpath - Set the path for log to write to
+        [string]$logpath
+    )
+    begin {
+        $mailboxes = Get-Mailbox -Filter {name -like "*#CR*"}
+        $user = Read-Host "What is the username or display name of the user that you would like to grant full access to all mailboxes?"
+        $logpath = Read-Host "Enter the path you would like to save the log results to"
+    }
+    process {
         # Begin FOR loop
         ForEach($mailbox in $mailboxes){
             Add-MailboxPermission -Identity $mailbox.alias -User $user -AccessRights FullAccess
+            Write-Output "$user granted full access to $mailbox" | Out-File "$logpath\add-calendar-full-access-results.txt" -Append
         }
-    )
+    }
+    end {
+        Write-Output "Log written to $logpath"
+    }
+}
