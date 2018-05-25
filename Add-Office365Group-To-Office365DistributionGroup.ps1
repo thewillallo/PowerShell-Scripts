9fc29c63b4ea38aafd47be32a1594342b27856f2
thewillallo <39629482+thewillallo@users.noreply.github.com>
2018-05-25 15:40:57 -0400

    Multiple error corrections

--- Add-Office365Group-To-Office365DistributionGroup.ps1
+++ Add-Office365Group-To-Office365DistributionGroup.ps1
@@ -1,4 +1,4 @@
-function NestOffice365Group {
+function Add-Office365GroupToDistro {
     <#
     .SYNOPSIS
     This script will set allow you to nest an Office 365 Group in an Office 365 Distribution Group.
@@ -8,12 +8,18 @@ function NestOffice365Group {
     messages sent to a different Office 365 Distribution Group in your tenant. You will need to be logged into Exchange Online PowerShell in
     order to run this function.
     #>
-    param(
+    param
+    (
         # $distro - Prompts for the distribution group SMTP address
-        [string]$distro = Read-Host "Enter the SMTP address of the Office 365 Distribution Group"
+        [string]$distro,
         # $member - Prompts for the Office 365 Group SMTP address
-        [string]$member = Read-Host "Please enter the SMTP address of the Office 365 Group you would like to add to the $distro Distribution Group"
-
+        [string]$member
+    )
+    begin {
+        $distro = Read-Host "Enter the SMTP address of the Office 365 Distribution Group"
+        $member = Read-Host "Please enter the SMTP address of the Office 365 Group you would like to add to the $distro Distribution Group"
+    }
+    process {
         Do {
             # Command to set the distribution group
             Add-DistributionGroupMember -Identity $distro -Member $member
@@ -21,6 +27,10 @@ function NestOffice365Group {
             $question = Read-Host "Would you like to add more Office 365 groups to this distribution group? (Yes or No)"
             # End DO loop, will terminate if response to $question does not equal "Yes"
         } Until (($question -ne "Yes") -or ($question -ne "yes"))
+    }
+    end {
+        Write-Output "Script is complete"
+    }
 }
 
 
